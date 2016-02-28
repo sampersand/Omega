51ab1b8e23f6ba54b8fd7d9a9e58bcc2f52dc74b
westerhack <westerhack@gmail.com>
2016-02-28 01:19:15 -0800

    more tweaking to disp

--- Objects/FuncObj.py
+++ Objects/FuncObj.py
@@ -23,44 +23,17 @@ class funcobj(methodobj):
         if name == 'disp':
             dispargs, sep, end = [''], ', ', '\n'
             if len(args) > 0:
-                def scrub(args, ldict):
-                    print(repr(args),repr(ldict))
-                    args.eval(ldict)
-                    return ldict.lastval.base.scrubstr(args.control)
-                dispargs = (scrub(args, ldict) for args in args[0])
+                def scrub(pdispargs, ldict):
+                    for disparg in pdispargs:
+                        disparg.eval(ldict)
+                        yield ldict.lastval.base.scrubstr(args.control)
+                dispargs = scrub(args[0], ldict)
                 if len(args) > 1:
                     if not args[1].base.isnull():
                         sep = args[1].base.scrubstr(args.control)
                     if len(args) > 2:
                         if not args[2].base.isnull():
                             end = args[2].base.scrubstr(args.control)
-                    
-            # if name == 'disp':
-            #     from obj import strobj
-            #     args, sep, end = ('', ), ', ', '\n'
-            #     if len(eles) > 0:
-            #         if __debug__:
-            #             assert not eles[0].base.isnull(), 'when would it be?'
-            #         if not eles[0].base.isnull():
-            #             if not eles[0]:
-            #                 eles[0].eval(ldict)
-            #                 args = (ldict.lv.base.strobj.scrub(), )
-            #             else:
-            #                 def scrub(ele, ldict):
-            #                     print(repr(ele),repr(ldict))
-            #                     ele.eval(ldict)
-            #                     return ldict.lv.base.strobj.scrub()
-            #                 args = [scrub(ele, ldict) for ele in eles[0]]
-            #         if len(eles) > 1:
-            #             if not eles[1].base.isnull():
-            #                 if __debug__:
-            #                     assert isinstance(eles[1].base, strobj)
-            #                 sep = eles[1].base.scrub()
-            #             if len(eles) > 2:
-            #                 if not eles[2].base.isnull():
-            #                     if __debug__:
-            #                         assert isinstance(eles[2].base, strobj)
-            #                     end = eles[2].base.scrub()
             print(*dispargs, sep = sep, end = end)
         else:
             raise SyntaxError("Unknown Function '{}' in arguments '{}'! Known Functions: {}".format(self, args, 
