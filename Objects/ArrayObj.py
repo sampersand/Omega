c457cde8eba31b40a22b8164214f330f71264efc
westerhack <westerhack@gmail.com>
2016-02-28 17:51:15 -0800

    i guess deepcopy is needed afterall..

--- Objects/ArrayObj.py
+++ Objects/ArrayObj.py
@@ -20,37 +20,40 @@ class arrayobj(obj):
         if not args:
             ldict.lastval = self
             return
-        name = str(args[0])
-        if name == 'get':
-            if __debug__:
-                assert len(args) == 2, 'array:get:pos'
-            args[1].eval(ldict)
-            ldict.lastval = self.base[ldict.lastval.base.base]
-        elif name == 'len':
-            if __debug__:
-                assert len(args) == 1, 'array:len'
-            from Group import group
-            from Objects.IntObj import intobj
-            ldict.lastval = group(base = intobj(len(self.base)))
-        elif name == 'add':
-            if __debug__:
-                assert len(args) == 2, 'array:add:(pos, ele) or array:add:(ele,) not '+ str(args)
-            if len(args[1]) == 2:
-                args[1][0].eval(ldict)
-                pos = ldict.lastval.base.base
+        try:
+            super().eval(args, ldict)
+        except SyntaxError:
+            name = str(args[0])
+            if name == 'get':
+                if __debug__:
+                    assert len(args) == 2, 'array:get:pos'
+                args[1].eval(ldict)
+                ldict.lastval = self.base[ldict.lastval.base.base]
+            elif name == 'len':
+                if __debug__:
+                    assert len(args) == 1, 'array:len'
+                from Group import group
+                from Objects.IntObj import intobj
+                ldict.lastval = group(base = intobj(len(self.base)))
+            elif name == 'add':
+                if __debug__:
+                    assert len(args) == 2, 'array:add:(pos, ele) or array:add:(ele,) not '+ str(args)
+                if len(args[1]) == 2:
+                    args[1][0].eval(ldict)
+                    pos = ldict.lastval.base.base
+                else:
+                    pos = -1
+                args[1][len(args[1]) == 2].eval(ldict)
+                self.base.insert(pos, ldict.lastval)
+                ldict.lastval = self.base[pos]
             else:
-                pos = -1
-            args[1][len(args[1]) == 2].eval(ldict)
-            self.base.insert(pos, ldict.lastval)
-            ldict.lastval = self.base[pos]
-        else:
-            raise SyntaxError("Unknown array function '{}' with parameters '{}'!".format(name,
-                len(args) >1 and args[1:] or ''))
-    #     if eles.base is self:
-    #         locls.lv = eles
-    #         return
-    #     import inbuiltfuncs
-    #     inbuiltfuncs.evalarray(self, eles, locls)
+                raise SyntaxError("Unknown array function '{}' with parameters '{}'!".format(name,
+                    len(args) >1 and args[1:] or ''))
+        #     if eles.base is self:
+        #         locls.lv = eles
+        #         return
+        #     import inbuiltfuncs
+        #     inbuiltfuncs.evalarray(self, eles, locls)
 
 
 
