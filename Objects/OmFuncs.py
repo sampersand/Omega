140ea253dec21ead14c8874e1299aee28c3dff6b
westerhack <westerhack@gmail.com>
2016-03-02 12:07:32 -0800

    this dictionary idea might work well with other functions too...

--- Objects/OmFuncs.py
+++ Objects/OmFuncs.py
@@ -8,7 +8,7 @@ def eval(self, args, ldict):
         raise SyntaxError("No known 'om' function '{}' with arguments '{}'!".\
                           format(name, '' if len(args) == 0 else args[1:]))
     else:
-        _definedfuncs[name](self, args, ldict)
+        _definedfuncs[name](self, args[1:], ldict)
 
 def _rand(self, args, ldict):
     ldict.last = args.newgroup(floatobj(random.random()))
@@ -16,8 +16,10 @@ def _rand(self, args, ldict):
 def _ldict(self, args, ldict):
     ldict.last = args.newgroup(dictobj(ldict.deepcopy()))
     del ldict.last.base.base.last
-
+def _itos(self, args, ldict):
+    assert 0, args
 _definedfuncs = {
-    'rand':_rand
-    'ldict':_ldict
+    'rand':_rand,
+    'ldict':_ldict,
+    'itos':_itos
 }
