6ff1ff14b44966ae1ea796ff8337c594d1937b82
westerhack <westerhack@gmail.com>
2016-03-10 20:22:23 -0800

    added _classiter, which gets the methods of a class

--- Objects/CollectionObjects/UClassObj.py
+++ Objects/CollectionObjects/UClassObj.py
@@ -1,8 +1,19 @@
 from Objects import collectionobj, mthdobj, obj
 from Group import group
 class uclassobj(collectionobj):
+
+    @staticmethod
+    def _classiter(cl):
+        for line in cl:
+            if isinstance(line.baseobj, mthdobj):
+                yield line
+
     def _evalargs(self, args, lcls):
         ret = super()._evalargs(args, lcls)
+        if len(args) == 1:
+            itr = uclassobj._classiter(lcls.iv.last)
+            assert 0, list(str(x) for x in itr)
+            return
         if ret != NotImplemented:
             return ret
         funcname = str(args[0])
@@ -12,11 +23,13 @@ class uclassobj(collectionobj):
             if isinstance(line.baseobj, mthdobj):
                 
                 lcls.iv.last = line
-                r = group(data = lcls.iv.last[0].datastr, baseobj = obj(), control = args.control, args = args[1:])
-                print(repr(r),"@aoihefoiahwef")
+                delim = args.control.delims['applier']
+                r = group(data = delim[0], baseobj = delim[1], control = args.control, args = args[1:])
+                quit('todo!')
                 quit(repr(lcls.iv.last))
                 r.evalgrp(lcls)
-                print(lcls)
+                # print(repr(r),"@aoihefoiahwef")
+                # print(lcls)
                 return 
                 # quit(lcls)
                 # return line.evalgrp(lcls)
